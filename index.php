130bff61b0209486064874425d9d4779d96d92cf
TwelveParsecs <davidmbrennan@outlook.com>
2016-03-24 21:47:46 -0400

    passing a php array to javascript
    -Added library for importing .csv

--- index.php
+++ index.php
@@ -4,18 +4,25 @@
     <meta charset="UTF-8">
     <title>Near Earth Objects</title>
     <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/d3.v3.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <script src="js/jquery-2.2.1.min.js"></script>
+    <script src="js/d3.min.js"></script>
   </head>
   <body>
+
     <?php
       require "key.php";//variable called $apiKey
 
       //get data from NASA Near Earth Objects
-      $url= "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=";
+      //$url= "https://api.nasa.gov/neo/rest/v1/feed?start_date=2015-09-07&end_date=2015-09-08&api_key=";
+      $url= "https://api.nasa.gov/neo/rest/v1/neo/browse/?api_key=";
 
-      //$apidata = json_decode(curlGet($url.$apiKey),true);
+      $apidata = json_decode(curlGet($url.$apiKey),true);
+      $apidata=$apidata["near_earth_objects"];
 
+      $asteroids = [];
+
+    
       dump($apidata);
 
       function dump($v,$t=""){
@@ -37,9 +44,28 @@
 
         return $output;
       }
-
-
     ?>
 
+    <div class="container">
+      <h2> Near Earth Objects </h2>
+      <section id="chart">
+        <div class="item">Star  </div>
+        <div class="item">Ship  </div>
+        <div class="item">Cup  </div>
+
+      </section>
+    </div>
+
+
+    <script>
+    /*
+      var data = <?php echo json_encode($apidata); ?>;
+      var dataArray = $.map(data, function(value, index) {
+        return [value];
+      });
+
+      console.log(dataArray[2][0])
+*/
+    </script>
   </body>
 </html>
